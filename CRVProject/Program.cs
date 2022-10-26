31d29bed7ee9d4b0deb2dcc70bb5330cf6302d5a
2max968 <maximilian-haag@web.de>
2022-10-26 09:51:57 +0200

    Update

--- CRVProject/Program.cs
+++ CRVProject/Program.cs
@@ -38,31 +38,67 @@ while (true)
     // HUE Shift
     else if (ind == 1)
     {
+        // Bild aus JPEG Datei laden
         Mat image = Cv2.ImRead("Image.jpg");
+
+        // Bild von RGB in HSV konvertieren
         Mat hsv = new Mat();
         Cv2.CvtColor(image, hsv, ColorConversionCodes.RGB2HSV);
 
+        // Original Bild anzeigen, damit ein Fenster erstellt wird
         Cv2.ImShow("Output", image);
-        Cv2.CreateTrackbar("HUE", "Output", 255, (pos, userData) =>
+
+        // Eine Trackbar zum Einstellen der HUE Verschiebung in dem Fenster "Output" erstellen
+        // Wenn der Wert der Trackbar geändert wird, wird die Lambda Funktion "(pos, userData) => {...}" aufgerufen
+        Cv2.CreateTrackbar("HUE", "Output", 255, (sliderValue, userData) =>
         {
+            // HSV Bild in die einzelnen Kanäle H/S/V aufsplitten
             Mat[] hsv_channels = Cv2.Split(hsv);
-            hsv_channels[0] += pos;
+
+            unsafe
+            {
+                // Auf jedes Pixel im H-channel den Wert der Trackbar aufaddieren
+                hsv_channels[0].ForEachAsByte((value, position) =>
+                {
+                    *value = (byte)(*value + sliderValue);
+                });
+            }
+
+            Cv2.ImShow("H", hsv_channels[0]);
+
+            // Die einzelnen Kanäle wieder zu einem einzigen Bild zusammenfügen
             Mat hsv_clone = new Mat();
             Cv2.Merge(hsv_channels, hsv_clone);
+
+            // Das zusammengesetzte neue HSV Bild zurück in RGB konvertieren
             Mat rgb = new Mat();
             Cv2.CvtColor(hsv_clone, rgb, ColorConversionCodes.HSV2RGB);
+
+            // Fertiges Bild anzeigen
             Cv2.ImShow("Output", rgb);
+
+            // Jede erstellte Matrix vom Arbeitsspeicher entfernen
+            foreach (var hsv_channel in hsv_channels)
+                hsv_channel.Dispose();
+            hsv_clone.Dispose();
         });
-        Cv2.SetTrackbarMax("HUE", "Output", 128);
-        Cv2.SetTrackbarMin("HUE", "Output", -128);
+
+        // Den bereich der Trackbar auf [-128, 128] setzten, die Funktion CreateTrackbar() kann nur
+        // den oberen Grenzwert setzen, der untere ist immer 0
+        //Cv2.SetTrackbarMax("HUE", "Output", 128);
+        //Cv2.SetTrackbarMin("HUE", "Output", -128);
 
         Cv2.WaitKey();
         Cv2.DestroyAllWindows();
+        hsv.Dispose();
     }
     // Show Webcam
     else if (ind == 2)
     {
+        // Ein Objekt des Typs VideoCapture erstellen. Mit dieser Klasse kann man Bilder aus Videos 
+        // oder von einer Kamera bekommen
         VideoCapture webcam = new VideoCapture();
+        // Die erste Kamera des PCs öffnen
         webcam.Open(0);
         Mat frame = new Mat();
         while (true)
